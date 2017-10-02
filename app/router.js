7e7c3a5b7f42fbe614df9455c573783246d41608
toelapiut <toelapiut7@gmail.com>
2017-10-02 21:56:02 +0300

    added files

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('patientinfo');
+  this.route('patientdisp');
 });
 
 export default Router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(){
     return this.store.findAll('patientInfo')
+  },
+  actions:{
+    showForm(params){
+    var newPatient =  this.store.createRecord('patientInfo',params);
+      newPatient.save();
+      this.transitionTo('index');
+    }
   }
 });
