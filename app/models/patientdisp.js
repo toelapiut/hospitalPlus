0f9ef23ea87764d7313cbbbf58f333dcc05093e3
Andy <andrew.muchiri97@gmail.com>
2017-10-03 12:38:45 +0300

    change in model

--- app/models/patientdisp.js
+++ app/models/patientdisp.js
@@ -1,31 +1,17 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-<<<<<<< HEAD:app/models/patientdisp.js
   dob: DS.attr(),
   emergencyNo: DS.attr(),
   firstName: DS.attr(),
   lastName: DS.attr(),
   phoneNo: DS.attr(),
   sex: DS.attr(),
-  status: DS.attr(),
-  location: DS.attr(),
-  checkIntimethis: DS.attr(),
   examiner: DS.attr(),
   followUp: DS.attr()
-=======
   checkIntime:DS.attr(),
-   checkOuttime:DS.attr(),
-   dob: DS.attr(),
-   emergencyNo: DS.attr(),
-   examiner:DS.attr(),
-   firstName:DS.attr(),
-   followUp:DS.attr(),
-   lastName:DS.attr(),
+  checkOuttime:DS.attr(),
    location:DS.attr(),
    patientsId: DS.attr(),
-   phoneNo:DS.attr(),
-   sex:DS.attr(),
    status: DS.attr()
->>>>>>> 483ad0e1e23bbc4fcdfa96ecd713cd190bafd070:app/models/patient-info.js
 });
