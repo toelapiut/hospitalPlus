c0dd18c819f8de1f6ba140253ec62062c21e06ac
toelapiut <toelapiut7@gmail.com>
2017-10-04 18:08:09 +0300

    add

--- app/components/new-patient.js
+++ app/components/new-patient.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   showHideForm: false,
+  sex: null,
+  status: null,
+
   actions: {
     showForm() {
       this.set('showHideForm', true)
@@ -9,11 +12,21 @@ export default Ember.Component.extend({
     hidePatientForm() {
       this.set('showHideForm', false)
     },
+    setSelection: function(selected) {
+      this.set('sex', selected)
+      console.log(this.get('sex'))
+    },
+
+    setSelection2: function(selected2) {
+      this.set('status', selected2)
+      console.log(this.get('status'))
+    },
+
     submitForm() {
       var params = {
         checkIntime: this.get('checkIntime'),
-        checkOuttime:this.get('checkOuttime'),
-        patientsId:this.get("patientsId"),
+        checkOuttime: this.get('checkOuttime'),
+        patientsId: this.get("patientsId"),
         dob: this.get('dob'),
         emergencyNo: this.get('emergencyNo'),
         examiner: this.get('emergencyNo'),
@@ -25,6 +38,7 @@ export default Ember.Component.extend({
         sex: this.get('sex'),
         status: this.get('status')
       };
+      console.log(params)
       this.set('showHideForm', false);
       this.sendAction("submitForm", params)
     }
