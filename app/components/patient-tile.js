d12bd2fb36c363f8f5f24fa711cabafa42bab4b7
Andy <andrew.muchiri97@gmail.com>
2017-10-05 16:53:50 +0300

    changes made in router

--- app/components/patient-tile.js
+++ app/components/patient-tile.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   checkInclick: false,
+  updatePatient: false,
   actions: {
     checkIn() {
       this.set("checkInclick",true)
     },
     checkOut() {
       this.set("checkInclick",false)
-    }
+    },
+    update(patientdisplay, params) {
+      this.sendAction('update', patientdisplay, params);
+    },
   }
 });
--- app/routes/patientdisplay.js
+++ app/routes/patientdisplay.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  });
+  model() {
+    return Ember.RSVP.hash({
+      medications: this.store.findAll('medication')
+    });
+  },
+  actions: {
+    saveMedication3(params) {
+      var newMedication = this.store.createRecord('medication', params);
+      newMedication.save();
+      this.transitionTo('patientdisplay');
+    }
+  }
+});
