5c14168b32d6fb90d9f1dcb597453a6032bd44ea
Granton Mchana <gmchana254@gmail.com>
2017-10-05 09:00:07 +0300

    insertions

--- app/models/lab.js
+++ app/models/lab.js
@@ -2,9 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   labType: DS.attr(),
-  labNotes: DS.attr(),
+  labNote: DS.attr(),
   dateRequested: DS.attr(),
   requestedBy: DS.attr(),
-  patientNames: DS.attr(),
-  testResults: DS.attr()
+  patientName: DS.attr(),
+  testResult: DS.attr()
 });
